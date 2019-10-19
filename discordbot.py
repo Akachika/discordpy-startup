9e7482d9382150c7d4c4332327ee6c32628dee47
akashi <akashi735@gmail.com>
2019-10-19 12:33:08 +0900

    tesPlease enter the commit message for your changes. Lines startingt enter the commit message for your changes. Lines startingPlease enter the commit message for your changes. Lines startingPlease enter the commit message for your changes. Lines startingPlease enter the commit message for your changes. Lines startingPlease enter the commit message

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import os
 import traceback
@@ -6,20 +7,35 @@ from datetime import datetime
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
+
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+       if message.content.startswith('/date'):
+        reply = datetime.date
+        await message.channel.send(reply)
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-async def date(ctx):
-    reply = datetime.date
-    await ctx.send(reply)
-    
+   
 
 
 bot.run(token)
