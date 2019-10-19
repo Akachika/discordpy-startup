5c6fec2c9f4fffdae149521838ec9a0d2a3520d0
akashi <akashi735@gmail.com>
2019-10-19 20:43:03 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -12,28 +12,7 @@ client = discord.Client()
 @client.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
-    
-@client.event
-async def on_message(message):
-    if message.content.startswith('/mkch'):
-        category_id = message.channel.category_id
-        category = message.guild.get_channel(category_id)
-        new_channel = await category.create_text_channel(name='new')
-        reply = f'{new_channel.mention} を作成しました'
-        await message.channel.send(reply)
-
-@client.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
-
-
+ 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
@@ -47,9 +26,9 @@ async def date(ctx):
     await ctx.send('hogehoge')
 
 @bot.command()
-async def c_id(ctx):
+async def cid(ctx):
     await ctx.send('hogehoge')
-    category_id = bot.channel.category_id
+    category_id = ctx.command.channel.category_id
     await ctx.send(category_id)
     
 
