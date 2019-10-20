7fb0c11c19cfb62385bc3aa91f268ae226538162
akashi <akashi735@gmail.com>
2019-10-20 21:34:55 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import traceback
-from datetime import datetime
+import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -23,10 +23,10 @@ async def ping(ctx):
 async def date(ctx):
     nowNum = datetime.datetime.now()
     await ctx.send(nowNum)
-    nowStr = datetime.strftime(nowNum,'%m月%d日')
+    nowStr = datetime.datetime.strftime(nowNum,'%m月%d日')
     await ctx.send(nowStr)
     nowNum + datetime.timedelta(days=10)
-    nowStr = datetime.strftime(nowNum,'%m月%d日')
+    nowStr = datetime.datetime.strftime(nowNum,'%m月%d日')
     await ctx.send(nowStr)
 
 @bot.command()
@@ -38,7 +38,7 @@ async def cid(ctx):
 async def mkch(ctx):
     category_id = ctx.channel.category_id
     category = ctx.guild.get_channel(category_id)
-    now = datetime.now().strftime('%m月%d日')
+    now = datetime.datetime.now().strftime('%m月%d日')
     new_channel = await category.create_text_channel(name=now)
     reply = f'{new_channel.mention} を作成しました'
     await ctx.send(reply)
@@ -48,9 +48,9 @@ async def weekch(ctx):
     category_id = ctx.channel.category_id  ##634406237395746816 団体戦のカテゴリID
     category = ctx.guild.get_channel(category_id)
     for i in range(7):
-        now_num = datetime.now()
+        now_num = datetime.datetime.now()
         now_num.date += i
-        now = datetime.now().strftime('%m月 %d日')
+        now = datetime.datetime.now().strftime('%m月 %d日')
         new_channel = await category.create_text_channel(name=now)
         reply = f'{new_channel.mention} を作成しました'
     await ctx.send(reply)
